aba3f2f897951cb9a1bde34de7f76761131b566b
Kleis Auke Wolthuizen <github@kleisauke.nl>
2018-03-11 15:04:26 +0100

    Add thumbnail + smartcrop example

---
+++ NetVips.Samples/Samples/Smartcrop.cs
@@ -0,0 +1,18 @@
+﻿namespace NetVips.Samples.Samples
+{
+    public class Smartcrop : ISample
+    {
+        public string Name => "Smartcrop";
+        public string Category => "Conversion";
+
+        public const string Filename = "images/equus_quagga.jpg";
+
+        public string Execute(string[] args)
+        {
+            var image = Image.Thumbnail(Filename, 300, height: 300, crop: "attention");
+            image.WriteToFile("smartcrop.jpg");
+
+            return "See smartcrop.jpg";
+        }
+    }
+}
---
+++ NetVips.Samples/Samples/Thumbnail.cs
@@ -0,0 +1,18 @@
+﻿namespace NetVips.Samples.Samples
+{
+    public class Thumbnail : ISample
+    {
+        public string Name => "Thumbnail";
+        public string Category => "Resample";
+
+        public const string Filename = "images/lichtenstein.jpg";
+
+        public string Execute(string[] args)
+        {
+            var image = Image.Thumbnail(Filename, 300, height: 300);
+            image.WriteToFile("thumbnail.jpg");
+
+            return "See thumbnail.jpg";
+        }
+    }
+}
