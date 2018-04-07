ab5908643595d6b69fbf9ff389dc42d156bfe214
Kleis Auke Wolthuizen <github@kleisauke.nl>
2018-04-07 19:56:05 +0200

    More documentation improvements

--- src/NetVips/VOption.cs
+++ src/NetVips/VOption.cs
@@ -9,24 +9,54 @@ namespace NetVips
     /// </summary>
     public class VOption : IEnumerable<KeyValuePair<string, object>>
     {
-        private Dictionary<string, object> internalDictionary = new Dictionary<string, object>();
+        private readonly Dictionary<string, object> _internalDictionary = new Dictionary<string, object>();
 
-        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => internalDictionary.GetEnumerator();
+        /// <summary>
+        /// Returns an enumerator that iterates through the <see cref="_internalDictionary" />.
+        /// </summary>
+        /// <returns>A <see cref="T:System.Collections.Generic.Dictionary`2.Enumerator"></see> structure for the <see cref="_internalDictionary" />.</returns>
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator() => _internalDictionary.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator() => internalDictionary.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _internalDictionary.GetEnumerator();
 
+        /// <summary>
+        /// Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the value to get or set.</param>
+        /// <returns>The value associated with the specified key.</returns>
         public object this[string key]
         {
-            get => internalDictionary[key];
+            get => _internalDictionary[key];
             set => Add(key, value);
         }
 
-        public int Count => internalDictionary.Count;
+        /// <summary>
+        /// Gets the number of key/value pairs contained in the <see cref="_internalDictionary" />.
+        /// </summary>
+        /// <returns>The number of key/value pairs contained in the <see cref="_internalDictionary" />.</returns>
+        public int Count => _internalDictionary.Count;
 
-        public void Add(string key, object value) => internalDictionary.Add(key, value);
+        /// <summary>
+        /// Adds the specified key and value to the <see cref="_internalDictionary" />.
+        /// </summary>
+        /// <param name="key">The key of the element to add.</param>
+        /// <param name="value">The value of the element to add. The value can be null for reference types.</param>
+        /// <returns></returns>
+        public void Add(string key, object value) => _internalDictionary.Add(key, value);
 
-        public bool Remove(string key) => internalDictionary.Remove(key);
+        /// <summary>
+        /// Removes the value with the specified key from the <see cref="_internalDictionary" />.
+        /// </summary>
+        /// <param name="key">The key of the element to remove.</param>
+        /// <returns><see langword="true" /> if the element is successfully found and removed; otherwise, <see langword="false" />.</returns>
+        public bool Remove(string key) => _internalDictionary.Remove(key);
 
-        public bool TryGetValue(string key, out object value) => internalDictionary.TryGetValue(key, out value);
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the value to get.</param>
+        /// <param name="value">When this method returns, contains the value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter. This parameter is passed uninitialized.</param>
+        /// <returns><see langword="true" /> if the <see cref="_internalDictionary" /> contains an element with the specified key; otherwise, <see langword="false" />.</returns>
+        public bool TryGetValue(string key, out object value) => _internalDictionary.TryGetValue(key, out value);
     }
 }
--- src/NetVips/VipsException.cs
+++ src/NetVips/VipsException.cs
@@ -9,15 +9,28 @@ namespace NetVips
     /// </summary>
     public class VipsException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VipsException" /> class.
+        /// </summary>
         public VipsException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VipsException" /> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public VipsException(string message) : base($"{message}{Environment.NewLine}{VipsErrorBuffer()}")
         {
             Vips.VipsErrorClear();
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VipsException" /> class with a specified error message 
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
         public VipsException(string message, Exception inner) : base(
             $"{message}{Environment.NewLine}{VipsErrorBuffer()}", inner)
         {
