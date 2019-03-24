0b65df361dea9b549092ab97f6c7ee9b2fda79f2
Kleis Auke Wolthuizen <github@kleisauke.nl>
2019-03-24 14:14:52 +0100

    Add image pyramid example
    See #31.

---
+++ samples/NetVips.Samples/Samples/ImagePyramid.cs
@@ -0,0 +1,23 @@
+﻿namespace NetVips.Samples
+{
+    public class ImagePyramid : ISample
+    {
+        public string Name => "Image Pyramid";
+        public string Category => "Create";
+
+        public const int TileSize = 50;
+        public const string Filename = "images/sample2.v";
+
+        public string Execute(string[] args)
+        {
+            // Build test image
+            var im = Image.NewFromFile(Filename, access: Enums.Access.Sequential);
+            im = im.Replicate(TileSize, TileSize);
+
+            // Save image pyramid
+            im.Dzsave("images/image-pyramid");
+
+            return "See images/image-pyramid.dzi";
+        }
+    }
+}
