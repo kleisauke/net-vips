f7e514344e83eef00a4f228acf17dd13480c99df
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-01-07 21:02:51 +0100

    Bump Travis libvips to 8.9.0
    + ensure libwebp >= 0.6 is installed on Travis.

--- build/travis.sh
+++ build/travis.sh
@@ -5,7 +5,13 @@ set -e
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     uname -a
 
+    # add support for WebP
+    wget http://archive.ubuntu.com/ubuntu/pool/main/libw/libwebp/libwebp-dev_0.6.1-2_amd64.deb
+    wget http://archive.ubuntu.com/ubuntu/pool/main/libw/libwebp/libwebpdemux2_0.6.1-2_amd64.deb
+    wget http://archive.ubuntu.com/ubuntu/pool/main/libw/libwebp/libwebpmux3_0.6.1-2_amd64.deb
+    wget http://archive.ubuntu.com/ubuntu/pool/main/libw/libwebp/libwebp6_0.6.1-2_amd64.deb
+    sudo dpkg -i *.deb
+
     . $TRAVIS_BUILD_DIR/build/install-vips.sh \
-      --skip-cache \
       --disable-dependency-tracking
 fi
