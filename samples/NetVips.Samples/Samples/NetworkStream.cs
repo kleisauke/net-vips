2b8d25d330373a1dee44d346121671db12181d8b
Kleis Auke Wolthuizen <github@kleisauke.nl>
2020-12-20 14:51:18 +0100

    Extend sample to test read loops (#101)
    This was fixed in libvips 8.10.3.

--- samples/NetVips.Samples/Samples/NetworkStream.cs
+++ samples/NetVips.Samples/Samples/NetworkStream.cs
@@ -9,19 +9,37 @@ namespace NetVips.Samples
         public string Name => "Network stream";
         public string Category => "Streaming";
 
-        public const string Uri = "https://images.weserv.nl/zebra.jpg";
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/alpha-layer-2-ink.jpg"; // JPG
+        public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/alpha-layer-1-fill.png"; // PNG
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/dancing_banana2.lossless.webp"; // WebP
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/dancing-banana.gif"; // GIF
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/tv-test-pattern-146649.svg"; // SVG
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/winter_1440x960.heic"; // HEIC
+        //public const string Uri = "https://github.com/weserv/images/raw/5.x/test/api/fixtures/84y2.hdr"; // HDR
+
+        // -1 to test https://github.com/kleisauke/net-vips/issues/101
+        public const int BufferSize = 4096 - 1;
 
         public string Execute(string[] args)
         {
             using var web = new WebClient();
-            using (var stream = web.OpenRead(Uri))
-            {
-                var image = Image.NewFromStream(stream, access: Enums.Access.Sequential);
-                Console.WriteLine(image.ToString());
+            using var stream = web.OpenRead(Uri);
 
-                using var output = File.OpenWrite("stream-network.jpg");
-                image.WriteToStream(output, ".jpg");
-            }
+            var source = new SourceCustom();
+            source.OnRead += (buffer, length) =>
+            {
+                Console.WriteLine($"-> {length} bytes");
+                var bytesRead = stream.Read(buffer, 0, length > BufferSize ? BufferSize : length);
+                Console.WriteLine($"<- {bytesRead} bytes");
+                return bytesRead;
+            };
+
+            // var image = Image.NewFromStream(stream, access: Enums.Access.Sequential);
+            var image = Image.NewFromSource(source, access: Enums.Access.Sequential);
+            Console.WriteLine(image.ToString());
+
+            using var output = File.OpenWrite("stream-network.jpg");
+            image.WriteToStream(output, ".jpg");
 
             return "See stream-network.jpg";
         }
