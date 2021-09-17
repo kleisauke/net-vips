087f9f7245b425285d548847d2f6f2c9abef28e3
Kleis Auke Wolthuizen <github@kleisauke.nl>
2021-09-17 13:56:42 +0200

    Switch back to "PreserveNewest" (#140)
    Since this behavior cannot be easily overridden (unless you
    manually modify the `.targets` file).

--- build/Build.cs
+++ build/Build.cs
@@ -182,7 +182,7 @@ partial class Build : NukeBuild
         {
             // Build the architecture specific packages
             NuGetPack(c => c
-                .SetVersion(VipsVersion + ".1")
+                .SetVersion(VipsVersion + ".2")
                 .SetOutputDirectory(ArtifactsDirectory)
                 .AddProperty("NoWarn", "NU5128")
                 .CombineWith(NuGetArchitectures,
@@ -192,7 +192,7 @@ partial class Build : NukeBuild
             // Build the all-in-one package, which depends on the previous packages.
             NuGetPack(c => c
                 .SetTargetPath(RootDirectory / "build/native/NetVips.Native.nuspec")
-                .SetVersion(VipsVersion + ".1")
+                .SetVersion(VipsVersion + ".2")
                 .SetOutputDirectory(ArtifactsDirectory)
                 .AddProperty("NoWarn", "NU5128"));
         });
