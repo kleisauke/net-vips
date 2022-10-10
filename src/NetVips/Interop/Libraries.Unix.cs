3ebac038aee6506a402df6c70de8b51f87c2d8fa
Kleis Auke Wolthuizen <github@kleisauke.nl>
2022-10-10 10:39:14 +0200

    Ensure compatibility with FreeBSD and variants

--- src/NetVips/Interop/Libraries.Unix.cs
+++ src/NetVips/Interop/Libraries.Unix.cs
@@ -3,7 +3,7 @@ namespace NetVips.Interop
     internal static partial class Libraries
     {
         // We can safely define all these variables as `libvips.so.42` since
-        // DLLImport uses dlsym() on Linux. This function also searches for named
+        // DLLImport uses dlsym() on *nix. This function also searches for named
         // symbols in the dependencies of the shared library. Therefore, we can
         // provide libvips as a single shared library with all dependencies
         // statically linked without breaking compatibility with shared builds
