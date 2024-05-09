kleisauke/net-vips

commit bf6b7b585b70ac04687240f577be7e5ea02cd76d

Ensure code is AOT-friendly (#196)

src/NetVips/Image.cs
@@ -25,6 +25,11 @@ namespace NetVips
         /// </remarks>
         public delegate void EvalDelegate(Image image, VipsProgress progressStruct);
 
+        /// <summary>
+        /// Internal marshaller delegate for <see cref="EvalDelegate"/>
+        /// </summary>
+        internal delegate void EvalMarshallDelegate(IntPtr imagePtr, VipsProgress progress, IntPtr userDataPtr);
+
         /// <inheritdoc cref="VipsObject"/>
         internal Image(IntPtr pointer)
             : base(pointer)
@@ -2092,14 +2097,27 @@ namespace NetVips
         /// <exception cref="T:System.ArgumentException">If it failed to connect the signal.</exception>
         public ulong SignalConnect(Enums.Signals signal, EvalDelegate callback, IntPtr data = default)
         {
+            void EvalMarshal(IntPtr imagePtr, VipsProgress progress, IntPtr userDataPtr)
+            {
+                if (imagePtr == IntPtr.Zero)
+                {
+                    return;
+                }
+
+                using var image = new Image(imagePtr);
+                image.ObjectRef();
+
+                callback.Invoke(image, progress);
+            }
+
             switch (signal)
             {
                 case Enums.Signals.PreEval:
-                    return SignalConnect("preeval", callback, data);
+                    return SignalConnect<EvalMarshallDelegate>("preeval", EvalMarshal, data);
                 case Enums.Signals.Eval:
-                    return SignalConnect("eval", callback, data);
+                    return SignalConnect<EvalMarshallDelegate>("eval", EvalMarshal, data);
                 case Enums.Signals.PostEval:
-                    return SignalConnect("posteval", callback, data);
+                    return SignalConnect<EvalMarshallDelegate>("posteval", EvalMarshal, data);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(signal), signal,
                         $"The value of argument '{nameof(signal)}' ({signal}) is invalid for enum type '{nameof(Enums.Signals)}'.");

src/NetVips/Internal/Vips.cs
@@ -17,8 +17,8 @@ namespace NetVips.Internal
         internal delegate IntPtr TypeMap2Fn(IntPtr type, IntPtr a, IntPtr b);
 
         [SuppressUnmanagedCodeSecurity, UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate IntPtr ArgumentMapFn(IntPtr @object, IntPtr pspec, IntPtr argumentClass,
-            IntPtr argumentInstance, IntPtr a, IntPtr b);
+        internal delegate IntPtr ArgumentMapFn(IntPtr @object, GParamSpec.Struct pspec, VipsArgumentClass argumentClass,
+            VipsArgumentInstance argumentInstance, IntPtr a, IntPtr b);
 
         [SuppressUnmanagedCodeSecurity, UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate int CallbackFn(IntPtr a, IntPtr b);

src/NetVips/Introspect.cs
@@ -189,17 +189,16 @@ namespace NetVips
             }
             else
             {
-                IntPtr AddConstruct(IntPtr self, IntPtr pspec, IntPtr argumentClass, IntPtr argumentInstance,
-                    IntPtr a,
-                    IntPtr b)
+                IntPtr AddConstruct(IntPtr self, GParamSpec.Struct pspec, VipsArgumentClass argumentClass,
+                    VipsArgumentInstance argumentInstance, IntPtr a, IntPtr b)
                 {
-                    var flags = argumentClass.Dereference<VipsArgumentClass>().Flags;
+                    var flags = argumentClass.Flags;
                     if ((flags & Enums.ArgumentFlags.CONSTRUCT) == 0)
                     {
                         return IntPtr.Zero;
                     }
 
-                    var name = Marshal.PtrToStringAnsi(pspec.Dereference<GParamSpec.Struct>().Name);
+                    var name = Marshal.PtrToStringAnsi(pspec.Name);
 
                     AddArg(name, flags);
 

src/NetVips/ExtensionMethods.cs
@@ -48,7 +48,7 @@ namespace NetVips
         /// <returns>A newly allocated managed object of the specified type.</returns>
         internal static T Dereference<T>(this IntPtr ptr)
         {
-            return (T)Marshal.PtrToStructure(ptr, typeof(T));
+            return Marshal.PtrToStructure<T>(ptr);
         }
 
         /// <summary>

src/NetVips/GObject.cs
@@ -55,33 +55,15 @@ namespace NetVips
         /// <remarks>
         /// The callback will be triggered every time this signal is issued on this instance.
         /// </remarks>
+        /// <typeparam name="T">The type of the callback to connect.</typeparam>
         /// <param name="detailedSignal">A string of the form "signal-name::detail".</param>
         /// <param name="callback">The callback to connect.</param>
         /// <param name="data">Data to pass to handler calls.</param>
         /// <returns>The handler id.</returns>
         /// <exception cref="T:System.ArgumentException">If it failed to connect the signal.</exception>
-        public ulong SignalConnect(string detailedSignal, Delegate callback, IntPtr data = default)
+        public ulong SignalConnect<T>(string detailedSignal, T callback, IntPtr data = default)
+            where T : notnull
         {
-            if (callback is Image.EvalDelegate evalDelegate)
-            {
-                void EvalMarshal(IntPtr imagePtr, IntPtr progressPtr, IntPtr userDataPtr)
-                {
-                    if (progressPtr == IntPtr.Zero || imagePtr == IntPtr.Zero)
-                    {
-                        return;
-                    }
-
-                    using var image = new Image(imagePtr);
-                    image.ObjectRef();
-
-                    var progressStruct = progressPtr.Dereference<VipsProgress>();
-
-                    evalDelegate.Invoke(image, progressStruct);
-                }
-
-                callback = (VipsImage.EvalSignal)EvalMarshal;
-            }
-
             // add a weak reference callback to ensure all handles are released on finalization
             if (_handles.Count == 0)
             {
@@ -95,7 +77,8 @@ namespace NetVips
             var delegateHandle = GCHandle.Alloc(callback);
             _handles.Add(delegateHandle);
 
-            var ret = GSignal.ConnectData(this, detailedSignal, callback, data, null, default);
+            var cHandler = Marshal.GetFunctionPointerForDelegate(callback);
+            var ret = GSignal.ConnectData(this, detailedSignal, cHandler, data, null, default);
             if (ret == 0)
             {
                 throw new ArgumentException("Failed to connect signal " + detailedSignal);

src/NetVips/Internal/GObject.cs
@@ -249,7 +249,7 @@ namespace NetVips.Internal
             EntryPoint = "g_signal_connect_data")]
         internal static extern ulong ConnectData(GObjectManaged instance,
             [MarshalAs(UnmanagedType.LPStr)] string detailedSignal,
-            [MarshalAs(UnmanagedType.FunctionPtr)] Delegate cHandler, IntPtr data,
+            IntPtr cHandler, IntPtr data,
             GClosureNotify destroyData, Enums.GConnectFlags connectFlags);
 
         [SuppressUnmanagedCodeSecurity]

src/NetVips/NetVips.csproj
@@ -14,6 +14,8 @@
     <DefaultItemExcludes>$(DefaultItemExcludes);Interop\Libraries.*.cs</DefaultItemExcludes>
     <AppendRuntimeIdentifierToOutputPath>false</AppendRuntimeIdentifierToOutputPath>
     <AppendTargetFrameworkToOutputPath>false</AppendTargetFrameworkToOutputPath>
+    <EnableTrimAnalyzer>true</EnableTrimAnalyzer>
+    <EnableAotAnalyzer>true</EnableAotAnalyzer>
   </PropertyGroup>
 
   <!-- Give an initial value based on the operating system where it's currently running on. -->

commit 839c22a7a9eacf43983003121bc50c5f692b4fbb

Don't assume pointers are auto-dereferenced
Fixes a regression introduced in commit bf6b7b5.

src/NetVips/Image.cs
@@ -30,7 +30,7 @@ namespace NetVips
         /// Internal marshaller delegate for <see cref="EvalDelegate"/>.
         /// </summary>
         [SuppressUnmanagedCodeSecurity, UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate void EvalMarshalDelegate(IntPtr imagePtr, VipsProgress progress, IntPtr userDataPtr);
+        internal delegate void EvalMarshalDelegate(IntPtr imagePtr, IntPtr progressPtr, IntPtr userDataPtr);
 
         /// <inheritdoc cref="VipsObject"/>
         internal Image(IntPtr pointer)
@@ -2103,9 +2103,9 @@ namespace NetVips
         /// <exception cref="T:System.ArgumentException">If it failed to connect the signal.</exception>
         public ulong SignalConnect(Enums.Signals signal, EvalDelegate callback, IntPtr data = default)
         {
-            void EvalMarshal(IntPtr imagePtr, VipsProgress progress, IntPtr userDataPtr)
+            void EvalMarshal(IntPtr imagePtr, IntPtr progressPtr, IntPtr userDataPtr)
             {
-                if (imagePtr == IntPtr.Zero)
+                if (imagePtr == IntPtr.Zero || progressPtr == IntPtr.Zero)
                 {
                     return;
                 }
@@ -2113,6 +2113,8 @@ namespace NetVips
                 using var image = new Image(imagePtr);
                 image.ObjectRef();
 
+                var progress = Marshal.PtrToStructure<VipsProgress>(progressPtr);
+                
                 callback.Invoke(image, progress);
             }
 

src/NetVips/Internal/Vips.cs
@@ -17,8 +17,8 @@ namespace NetVips.Internal
         internal delegate IntPtr TypeMap2Fn(IntPtr type, IntPtr a, IntPtr b);
 
         [SuppressUnmanagedCodeSecurity, UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        internal delegate IntPtr ArgumentMapFn(IntPtr @object, GParamSpec.Struct pspec, VipsArgumentClass argumentClass,
-            VipsArgumentInstance argumentInstance, IntPtr a, IntPtr b);
+        internal delegate IntPtr ArgumentMapFn(IntPtr @object, IntPtr pspec, IntPtr argumentClass,
+            IntPtr argumentInstance, IntPtr a, IntPtr b);
 
         [SuppressUnmanagedCodeSecurity, UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate int CallbackFn(IntPtr a, IntPtr b);

src/NetVips/Introspect.cs
@@ -189,16 +189,16 @@ namespace NetVips
             }
             else
             {
-                IntPtr AddConstruct(IntPtr self, GParamSpec.Struct pspec, VipsArgumentClass argumentClass,
-                    VipsArgumentInstance argumentInstance, IntPtr a, IntPtr b)
+                IntPtr AddConstruct(IntPtr self, IntPtr pspec, IntPtr argumentClass, IntPtr argumentInstance,
+                    IntPtr a, IntPtr b)
                 {
-                    var flags = argumentClass.Flags;
+                    var flags = Marshal.PtrToStructure<VipsArgumentClass>(argumentClass).Flags;
                     if ((flags & Enums.ArgumentFlags.CONSTRUCT) == 0)
                     {
                         return IntPtr.Zero;
                     }
 
-                    var name = Marshal.PtrToStringAnsi(pspec.Name);
+                    var name = Marshal.PtrToStringAnsi(Marshal.PtrToStructure<GParamSpec.Struct>(pspec).Name);
 
                     AddArg(name, flags);
 

src/NetVips/VipsProgress.cs
@@ -22,7 +22,8 @@ namespace NetVips
         /// <summary>
         /// Is the timer currently active?
         /// </summary>
-        public int Active;
+        [MarshalAs(UnmanagedType.I1)]
+        public bool Active;
     }
 
     /// <summary>
@@ -65,6 +66,11 @@ namespace NetVips
         /// <summary>
         /// Start time.
         /// </summary>
-        public GTimer Start;
+        private IntPtr StartPtr;
+
+        /// <summary>
+        /// Start time.
+        /// </summary>
+        public GTimer Start => Marshal.PtrToStructure<GTimer>(StartPtr);
     }
 }